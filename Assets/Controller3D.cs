fcd3c92e4e14666b3da4f53d73a3df841b3f69ac
bugzslayer <crisitoadvincula@gmail.com>
2020-06-27 23:43:49 +0800

    Bomb Generation
    The character can now place bombs;
    Also a wall has been created in the starting point.

--- Assets/Controller3D.cs
+++ Assets/Controller3D.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class Controller3D : MonoBehaviour
@@ -10,35 +12,37 @@ public class Controller3D : MonoBehaviour
     public float playerSpeed = 2.0f;
     public float jumpHeight = 1.0f;
     public float gravityValue = -9.81f;
-   // public Transform cam;
+    public GameObject bombs;
+    // public Transform cam;
     private void Start()
     {
         controller = gameObject.AddComponent<CharacterController>();
     }
 
-    void Update()
-    {
+    void Update(){
         groundedPlayer = controller.isGrounded;
-        if (groundedPlayer && playerVelocity.y < 0)
-        {
+        if (groundedPlayer && playerVelocity.y < 0){
             playerVelocity.y = 0f;
         }
 
         Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         controller.Move(move * Time.deltaTime * playerSpeed);
 
-        if (move != Vector3.zero)
-        {
+        if (move != Vector3.zero){
             gameObject.transform.forward = move;
         }
 
         // Changes the height position of the player..
-        if (Input.GetButtonDown("Jump") && groundedPlayer)
-        {
+        if (Input.GetButtonDown("Jump") && groundedPlayer){
             playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
         }
 
         playerVelocity.y += gravityValue * Time.deltaTime;
         controller.Move(playerVelocity * Time.deltaTime);
+
+        if (Input.GetButtonDown("Fire2") && bombs != null && groundedPlayer ) {
+            GameObject newBombs = Instantiate(bombs, new Vector3(transform.position.x, transform.position.y, transform.position.z),Quaternion.identity);
+            newBombs.SetActive(true);
+        }
     }
 }
